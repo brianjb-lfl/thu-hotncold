167693c73ee985785ebcd7eb3ed67574ae667295
Brian Besaw <bjbesaw@outlook.com>
2017-10-12 12:11:50 -0400

    added reducer testing for no action passed

---
+++ src/reducer.test.js
@@ -0,0 +1,44 @@
+import reducer from './reducer';
+import {newGame, makeGuess, toggleInfoModal} from './actions';
+
+describe('reducer', () => {
+  it('Should initialize state when nothing passed', () => {
+    const state = undefined;
+    const action = {
+      type: '__DUMMY'
+    }
+    const newState = reducer(state, action);
+    expect(newState.guesses.length).toEqual(0);
+    expect(newState.feedback).toEqual('Make your guess!');
+    expect(newState.showInfoModal).toEqual(false);
+    expect(newState.correctAnswer).toBeLessThanOrEqual(100);
+    expect(newState.correctAnswer).toBeGreaterThanOrEqual(0);
+  });
+
+
+
+
+});
+
+
+
+
+
+
+
+
+//NOTHING PASSED
+// initializes state when nothing passed
+
+//UNKNOWN PASSED
+// returns current state when unknown action passed
+
+// NEW_GAME
+  // updates state correctly
+
+// MAKE_GUESS
+  // handles NaN
+  // test input - get right feedback, and adds guess to array
+
+// TOGGLE_INFO_MODAL
+// test that infoModal toggles state.showInfoModal

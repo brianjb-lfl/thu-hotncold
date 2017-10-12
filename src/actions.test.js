83d07fd2c3c97fc20d516db4794cd1d1f7937610
giri68 <giri.pdt@gmail.com>
2017-10-12 11:16:11 -0700

    Added makeGuess and toggleInfoModal test to action

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,13 +1,34 @@
-import {newGame, makeGuess, toggleInfoModal} from './actions';
+import {newGame, makeGuess, toggleInfoModal, MAKE_GUESS, TOGGLE_INFO_MODAL, NEW_GAME} from './actions';
 
 describe('newGame testing',() => {
-  it('should return the action', ()=> {
+  it('should return the newGame action', ()=> {
     const action = newGame();
     expect(action.type).toEqual(
-       'NEW_GAME'
+       NEW_GAME
     );
     expect(action.correctAnswer).toBeLessThanOrEqual(100);
     expect(action.correctAnswer).toBeGreaterThanOrEqual(0);
   
+  })
+})
+
+describe('makeGuess testing', () => {
+  it('should return the makeGuess action', () => {
+    const number = 5;
+    const action = makeGuess(number);
+    expect(action).toEqual({
+      type: MAKE_GUESS,
+      guess: number
+    })
+
+  })
+})
+
+describe ('toggleInfoModal testing', () => {
+  it('should return toggleInfoModal action', () => {
+    const action = toggleInfoModal();
+    expect(action).toEqual({
+      type: TOGGLE_INFO_MODAL
+    })
   })
 })
